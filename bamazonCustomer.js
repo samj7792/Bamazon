adaf0846000c3ae46e077ce9a7fb149573965fbf
Sam Jackson <samuelsjackson7@gmail.com>
2019-07-15 21:15:49 -0400

    prompt user which item they would like to buy

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var columnify = require("columnify");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,14 +17,19 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
+
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection1();
+
+  // console.log("connected as id " + connection.threadId);
+
+  displayProducts();
+
+  connection.end();
 });
 
 var sequel = "SELECT * FROM products";
 
-function afterConnection1() {
+function displayProducts() {
   connection.query(sequel, function(err, res) {
 
     if (err) throw err;
@@ -31,11 +37,40 @@ function afterConnection1() {
     // console.log(res);
 
     var columns = columnify(res, {
-      columnSplitter: ' | '
+      columnSplitter: ' | ',
+      paddingChr: '.',
     });
-     
+
+    // Display table of products
+    console.log("Here is what is in stock:\n");
+    
     console.log(columns);
 
-    connection.end();
+    var itemIds = [];
+
+    for (var i = 0; i < res.length; i++) {
+      itemIds.push(res[i].item_id);
+    }
+
+    // Ask user what they would like to buy
+    inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to buy (by ITEM_ID)?",
+        choices: itemIds,
+        name: "purchase"
+      }
+    ])
+    .then(function(inquirerResponse){
+
+      if (inquirerResponse.transport) {
+        
+      }
+
+    })
+
+    // connection.end();
   });
 }
+
