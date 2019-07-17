c3d779cf897df3f32e97a2916753e7487c29acb9
Sam Jackson <samuelsjackson7@gmail.com>
2019-07-16 20:52:23 -0400

    added prompt with relation to switch statement containing dummy functions at the moment

--- bamazonManager.js
+++ bamazonManager.js
@@ -22,7 +22,36 @@ connection.connect(function(err) {
 
   console.log("connected as id " + connection.threadId);
 
-  
+  promptMngr();
 
   connection.end();
 });
+
+
+function promptMngr() {
+  inquirer.prompt ([
+      {
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
+        name: 'choice'
+      }
+    ])
+    .then(function(inqRes) {
+      switch(inqRes.choice) {
+        case 'View Products for Sale':
+            viewProds();
+            break;
+        case 'View Low Inventory':
+            lowInv();
+            break;
+        case 'Add to Inventory':
+            addInv();
+            break;
+        case 'Add New Product':
+            newProd();
+            break;
+      }
+    })
+}
+
